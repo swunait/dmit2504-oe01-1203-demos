709d36da3e366071d839f07b3729848830e31d06
Sam Wu <swu@nait.ca>
2021-05-21 18:20:15 -0600

    completed custom lsitview demo

--- ListViewDemo_CountryCaptial/app/src/main/java/ca/nait/dmit/listviewdemo_countrycapital/MainActivity.kt
+++ ListViewDemo_CountryCaptial/app/src/main/java/ca/nait/dmit/listviewdemo_countrycapital/MainActivity.kt
@@ -3,15 +3,33 @@ package ca.nait.dmit.listviewdemo_countrycapital
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
+import android.widget.ListView
 
 class MainActivity : AppCompatActivity() {
+
+    val countryEditText: EditText by lazy { findViewById(R.id.activity_main_countryEditText) }
+    val capitalEditText: EditText by lazy { findViewById(R.id.activity_main_capitalEditText) }
+    val countryListView: ListView by lazy { findViewById(R.id.activity_main_countryListView)}
+    val countryDataAdapter = CountryAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        countryListView.adapter = countryDataAdapter
     }
 
     fun onSubmitClick(view: View) {
+        val countryName = countryEditText.text.toString()
+        val capitalCityName = capitalEditText.text.toString()
+
+        val newCountry = Country(countryName, capitalCityName)
+        countryDataAdapter.addItem(newCountry)
+
+        // Clear the edit text fields to allow entry of another country
+        countryEditText.setText("")
+        capitalEditText.setText("")
 
     }
 }
